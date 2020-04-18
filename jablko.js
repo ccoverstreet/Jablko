9b4286a7878caa54a40a2ad50961c3afded2e06c
ccoverstreet <coverst2@vols.utk.edu>
2020-04-18 15:54:50 -0400

    Working on creating Web Interface server

--- jablko.js
+++ jablko.js
@@ -3,7 +3,38 @@
 // April 17, 2020
 // Jablko.js serves as a manager for all Jablko related processes and is responsible for monitoring I/O output for all subprocesses, restarting processes, identifying problems internally.
 
-const fork = require("child_process").fork // Used for forking submodules
+const jablko_root = __dirname;
+const child_options = {
+	silent: true
+};
 
+function jablko_log(process_name, data, options={color: "normal"}) {
+	var prefix = `${process_name} [${new Date().toLocaleString("sv-SE")}]: ` // Creating prefix for process log
+	const split_data_string = data.toString().split("\n"); // Split at newline to create proper indentation later on
 
+	var output_string = prefix;
+	for (var i = 0; i < split_data_string.length; i++) {
+		if (i == 0) {
+			output_string += split_data_string[i];
+		} 
+		output_string += "\nASD";
+		
+	}
+
+	console.log(output_string);
+		
+	console.log(options.color);
+}
+
+const fork = require("child_process").fork; // Used for forking submodules
+
+var jablko_web_interface = fork(`${jablko_root}/web_interface/jablko_web_interface.js`, child_options);
+
+jablko_web_interface.stdout.on("data", function(data) {
+	jablko_log("Jablko Web Interface", data);
+});
+
+jablko_web_interface.stderr.on("data", function(data) {
+	console.log(data.toString());
+});
 
--- web_interface/jablko_web_interface.js
+++ web_interface/jablko_web_interface.js
@@ -1,1 +1,19 @@
 // jablko_web_interface.js: NodeJS server that runs the web interface for Jablko
+// Cale Overstreet, Corinne Gerhold
+// April 18, 2020
+// Primary server for serving smart home web interface.
+
+const jablko_root = __dirname.replace("/web_interface", ""); // Root of the Jablko Repo
+const jablko_web_root = __dirname.replace("/web_interface", "/public_html"); // Root of web assets
+
+const express = require("express"); // Main express include
+
+const server = express(); // Creating server object
+
+server.use(express.static(`${jablko_web_root}`));
+
+server.listen(10230, function() {
+	console.log("Jablko Web Interface started on port 10230");
+});
+
+
