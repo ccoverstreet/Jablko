1272c700bcf2982f96b1bf1a69c1f15294ee19a9
ccoverstreet <coverst2@vols.utk.edu>
2020-05-24 00:03:23 -0400

    Added readline-sync and started work on a database creation system

--- database/create_database.js
+++ database/create_database.js
@@ -1,20 +1,53 @@
 const sqlite3 = require("sqlite3");
+const rl = require("readline-sync");
+const crypto = require("crypto");
+const fs = require("fs");
 
-const db = new sqlite3.Database("test.db", function(err) {
-	if (err) {
-		console.log(err.message);
-	} else {
-		console.log("Connected to database");
+while (true) {
+	var mode_selection = rl.question(`
+What do you want to do?
+ 1. Create New Jablko Database
+$ `);
+	switch (mode_selection) {
+		case "1":
+			console.log("Creation Mode Selected");
+			create_jablko_database();
+			break;
+		default:
+			console.log("Invalid Response. Response should be an integer value");
 	}
-});
+}
 
-db.serialize(function() {
-	db.run(`CREATE TABLE IF NOT EXISTS test(
-	primary_key INTEGER PRIMARY KEY,
-	date TEXT)`);
-	
-	db.run(`INSERT INTO test(date) VALUES("2020-04-03 00:00:00"), ("ASDASD")`);
-});
+function create_jablko_database() {
+	const database_filename = rl.question("Enter a filename for the database: ");
 
+	if (fs.existsSync(`./${database_filename}.db`)) {
+		const overwrite_confirmation = rl.question("The file already exists. Do you wish to overwrite? ");
+		if (overwrite_confirmation != "y") {
+			console.log("Cancelling database creation... Returning to mode selection.");
+			return;
+		} else {
+			const db = new sqlite3.Database("test.db", function(err) {
+				if (err) {
+					console.log(err.message);
+				} else {
+					console.log("Successfully created database file...\nCreating table for users");
 
-db.close();
+					db.serialize(function() {
+						db.run(`CREATE TABLE IF NOT EXISTS test(
+							primary_key INTEGER PRIMARY KEY,
+							username TEXT NOT NULL,
+							password TEXT NOT NULL,
+							first_name TEXT NOT NULL,
+							phone_number TEXT NOT NULL,
+							wakeup_time TEXT NOT NULL
+						)`);
+					});
+				}
+			});
+
+			db.close();
+		}
+	}
+}
+
