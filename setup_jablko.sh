9c9c3f9a22752dc921c363cdc3ec0ca9dcab98c3
ccoverstreet <coverst2@vols.utk.edu>
2020-05-24 22:26:23 -0400

    Added sms prefix

--- setup_jablko.sh
+++ setup_jablko.sh
@@ -9,6 +9,6 @@ read sms_email
 printf "\tEnter Gmail Password: "
 read sms_password
 
-echo -e "{\n\t\"database_name\": \"\",\n\t\"email\": \"$sms_email\",\n\t\"password\": \"$sms_password\"\n}" > jablko_sms_config.json
+echo -e "{\n\t\"database_name\": \"\",\n\t\"sms_email\": \"$sms_email\",\n\t\"sms_password\": \"$sms_password\"\n}" > jablko_sms_config.json
 
 
