38ba9122305c83a47e7a0109d0d39ecdbd01106d
ccoverstreet <coverst2@vols.utk.edu>
2020-08-05 11:46:44 -0400

    Actually finishing the Arduino code/examples now that I have my own network

---
+++ ESP8266_Arduino_Templates/blinky/blinky.ino
@@ -0,0 +1,63 @@
+#include <ESP8266WebServer.h>
+
+const char *ssid = "moviecouncil";
+const char *password = "RAAE!lectron11!";
+
+const unsigned int timeout = 2000; // Timeout time that kills client response if taking too long. prevents hanging or soft resets.
+
+
+//WiFiServer server(80); // Create and instance of WiFiServer open on Port 80.
+
+ESP8266WebServer server;
+
+int light_status = 0;
+
+void setup() {
+	Serial.begin(9600);
+	delay(1);
+	Serial.print("Starting up...\n");
+	
+	init_wifi();
+
+  server.on("/", [](){server.send(200, "text/plain","Hello World");});
+  server.on("/status", get_status);
+  server.on("/toggle_light", toggle_light);
+  server.begin();
+
+  pinMode(5, OUTPUT);
+}
+
+void init_wifi() {
+	// Initializes WiFi on ESP8266. Must check for failure.
+	WiFi.begin(ssid, password);
+	Serial.print("Connecting to WiFi...\n");
+
+	while (WiFi.status() != WL_CONNECTED) {
+		Serial.print("ERROR: Unable to Connect to WiFi. Trying Again...\n");
+		delay(5000); // delay for WiFi connection attempts
+	}
+
+	Serial.print("SUCCESS: Connected to WiFi.\n");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  server.handleClient();
+}
+
+void get_status() {
+  Serial.println("ONLINE");
+  server.send(200, "text/plain", "GOOD");
+}
+
+void toggle_light() {
+  if (light_status) {
+    digitalWrite(5, LOW);
+    light_status = 0;
+  } else {
+    digitalWrite(5, HIGH);
+    light_status = 1;
+  }
+
+  server.send(200, "text/plain", "Toggled Light");
+}
