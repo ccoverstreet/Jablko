6a25314c22a5138a575bd9f7de194e0c961148f9
ccoverstreet <coverst2@vols.utk.edu>
2020-08-12 20:00:08 -0400

    Preparing to move to bitbucket

--- jablko_modules/test/test.ts
+++ jablko_modules/test/test.ts
@@ -4,10 +4,23 @@ export function permission_level() {
 
 export async function generate_card() {
 	return `
+	<script>
+	const test = {
+		toggle_light: async function() {
+			const raw_response = await fetch("/jablko_modules/test/toggle_light", {method: "POST"})
+				.catch((error) => {
+					console.log(error);
+					return;	
+				});
+			const response = await raw_response.json();
+			console.log(response);
+		}
+	}
+	</script>
 	<div class="jablko_module_card">
 	<h1>Test</h1>
 	<hr>
-	<button onclick="fetch('/jablko_modules/test/toggle_light', {method: 'POST'})">Toggle Light</button>
+	<button onclick="test.toggle_light()">Toggle Light</button>
 	<h2>Toggle Light</h2>
 	</div>
 	`
@@ -17,13 +30,14 @@ export async function toggle_light(context: any) {
 	const raw_response: any = await fetch("http://10.0.0.87/toggle_light", {method: "POST"})
 		.catch((error) => {
 			console.log(error);
+			context.response.type = "json";
+			context.response.body = {status: "fail", message: "Unable to toggle"};
 			return;
 		});
 
 	if (raw_response != undefined) {
-		console.log(raw_response);
 		const response = await raw_response.json();	
-		console.log(response);
+
 		if (response.status == "good") {
 			context.response.type = "json";
 			context.response.body = {status: "good", message: "Toggled"};
