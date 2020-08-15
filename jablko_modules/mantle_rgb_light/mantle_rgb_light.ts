34cfc5e9deedb45628904fb6390ccbef8a554069
ccoverstreet <coverst2@vols.utk.edu>
2020-08-15 00:25:40 -0400

    Working on graceful error handling

--- jablko_modules/mantle_rgb_light/mantle_rgb_light.ts
+++ jablko_modules/mantle_rgb_light/mantle_rgb_light.ts
@@ -1,4 +1,5 @@
 import { readFileStr } from "../../src/util.ts";
+import { module_config } from "./config.ts";
 
 export function permission_level() {
 	return 1;
@@ -9,14 +10,23 @@ export async function generate_card() {
 }
 
 export async function set_rgba(context: any) {
-	const raw_response = await fetch("http://10.0.0.46/set_rgba", {
+	const raw_response = await fetch(`http://${module_config.controller_ip}/set_rgba`, {
 		method: "POST",
 		headers: {
 			"Accept": "application/json",
 			"Content-Type": "application/json"
 		},
 		body: JSON.stringify(context.json_data)
-	});
+	})
+		.catch(function(error) {
+
+		});
+	
+	if (raw_response.status < 200 && raw_response.status >= 300) {
+		// Error in contacting controller
+		context.response.type = "json";
+		context.response.body = {status: "fail", message: `Error contacting controller (HTTP ERROR ${raw_response.status})`}
+	}
 	//console.log(await raw_response.json());
 	context.response.type = "json";
 	context.response.body = {status: "good", message: "Tried to update RGB"};
