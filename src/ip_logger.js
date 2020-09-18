f86d12ff393afc9c504efa58f39f335dab21b845
ccoverstreet <coverst2@vols.utk.edu>
2020-09-18 12:57:47 -0400

    Changed ip loggin behavior

--- src/ip_logger.js
+++ src/ip_logger.js
@@ -27,7 +27,6 @@ module.exports.ip_logger_middleware = async (req, res, next) => {
 		ip_addresses[req.connection.remoteAddress]++;
 	} else {
 		ip_addresses[req.connection.remoteAddress] = 1;
-		report_new_ip(req.connection.remoteAddress);
 		write_log();
 		access_counter = 0;
 	}
@@ -48,19 +47,4 @@ function write_log() {
 			console.log("Unable to write to ip_addresses.log");
 			console.debug(error);
 		});
-}
-
-async function report_new_ip(remote_address) {
-	await fetch(`https://tools.keycdn.com/geo.json?host=${remote_address}`)
-		.then(async (data) => {
-			const response = await data.json();
-			jablko.messaging_system.send_message(`New access from ip "${remote_address}"
-At ${response.data.geo.region_name}, ${response.data.geo.country_name}
-Provider: "${response.data.geo.isp}"
-Request happened at ${new Date().toLocaleString("sv-SE")}`);
-		})
-		.catch((error) => {
-			console.log("Unable to reverse-lookup new-ip");
-			console.debug(error);
-		})
 }
--- src/user_authentication.js
+++ src/user_authentication.js
@@ -6,13 +6,28 @@
 
 const fs = require("fs").promises;
 const bcrypt = require("bcrypt");
+const fetch = require("node-fetch");
 
 const jablko = require("../jablko_interface.js");
+
+var login_history = {};
+try {
+	login_history = require("../log/login_history.json");
+} catch (error) {
+	console.log("./log/login_history.json not found");
+	console.debug(error);
+}
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 module.exports.user_authentication_middleware = async function(req, res, next) {
 	console.debug(req.originalUrl);
 	if (req.originalUrl == "/login") {
 		try {
+			await sleep(1000);
+
 			// Get hash from SQLite
 			const password_hash = (await jablko.user_db.get("SELECT password FROM users WHERE username=?", [req.body.username])).password;
 
@@ -21,30 +36,74 @@ module.exports.user_authentication_middleware = async function(req, res, next) {
 				// Password hash matches, generate random string and put in sqlite database
 				const random_string = await bcrypt.hash(Math.random().toString(36), 10);
 				res.cookie("key_1", random_string);
-				
+
 				jablko.user_db.run("INSERT INTO login_sessions (username, session_cookie, creation_time) VALUES (?, ?, ?)", [req.body.username, random_string, Date.now()])
 					.catch((error) => {
 						console.log(error);
 					});
-				
+
 				console.log(`User "${req.body.username}" has logged in`);
 				res.json({status: "good", message: "Logged In"});
+
+				// Add to login history
+				if (req.connection.remoteAddress in login_history) {
+					// Connection is already in history
+					if (req.body.username in login_history[req.connection.remoteAddress].access) {
+						login_history[req.connection.remoteAddress].access[req.body.username].count++
+					} else {
+						login_history[req.connection.remoteAddress].access[req.body.username].count = 1;
+					}
+
+					login_history[req.connection.remoteAddress].access[req.body.username].last_access = new Date().toLocaleString("sv-SE");
+				} else {
+					login_history[req.connection.remoteAddress] = {
+						access: {}
+					}
+
+					login_history[req.connection.remoteAddress].access[req.body.username] = {
+						count: 1,
+						last_access: new Date().toLocaleString("sv-SE")
+
+					}
+
+					const remote_address = req.connection.remoteAddress;
+
+					await fetch(`https://tools.keycdn.com/geo.json?host=${remote_address}`)
+						.then(async (data) => {
+							const response = await data.json();
+							jablko.messaging_system.send_message(`New login from ip "${remote_address}"
+User "${req.body.username}"
+At ${response.data.geo.region_name}, ${response.data.geo.country_name}
+Provider: "${response.data.geo.isp}"
+Request happened at ${new Date().toLocaleString("sv-SE")}`);
+						})
+						.catch((error) => {
+							console.log("Unable to reverse-lookup new-ip");
+							console.debug(error);
+						})
+
+				}
+
+				write_login_history();
 				return
 			} else {
 				throw new Error("Invalid credentials");
 			}
 		} catch (err) {
-			console.log(err);
+			// Log invalid login and store in file.
+			console.debug(err);
 			res.json({status: "fail", message: "Invalid Login"});
+
+			fs.appendFile("./log/failed_logins.log", `${req.connection.remoteAddress},${req.body.username},${new Date().toLocaleString("sv-SE")}\n`);
+
 			return;
 		}
-		
 	} else if (req.originalUrl == "/bot_callback") {
 		await next();
 	} else if (req.originalUrl.startsWith("/module_callback")) {
 		// Callback for Jablko Modules on local network. Checks if IPv4 matches pattern "10.0.0.*". Need to add configuration later
 		// There might be a better option to this.
-		
+
 		const split_ip = req.ip.split(":");
 		const IPv4 = split_ip[split_ip.length - 1];
 		console.log(IPv4);
@@ -87,4 +146,12 @@ module.exports.user_authentication_middleware = async function(req, res, next) {
 			await next();
 		}
 	}
+}
+
+function write_login_history() {
+	fs.writeFile("./log/login_history.json", JSON.stringify(login_history, null, 4))
+		.catch((error) => {
+			console.log("Unable to write to ip_addresses.log");
+			console.debug(error);
+		});
 }
