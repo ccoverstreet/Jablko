52d18f1409586c82f99f0ba22f3c89e69d3e68ee
ccoverstreet <coverst2@vols.utk.edu>
2020-10-23 13:54:01 -0400

    Testing memory usage and multiple plugins

---
+++ test_modules/interface_status2/interface_status.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"jablko/types"
+	"net/http"
+	"fmt"
+	"github.com/buger/jsonparser"
+)
+
+type intStatus struct {
+	name string
+	updateInterval int
+}
+
+func Initialize(instanceName string, configData []byte) (types.JablkoMod, error) {
+	fmt.Println("Initializing Interface Status Module\n")
+	fmt.Printf("%s\n", configData)
+
+	instance := new(intStatus) 
+
+	instance.name = instanceName
+
+	updateInt, err := jsonparser.GetInt(configData, "updateInterval")
+	if err != nil {
+		return nil, err
+	}
+
+	instance.updateInterval = int(updateInt)
+
+	return types.StructToMod(instance), nil
+}
+
+func (instance *intStatus) Card(*http.Request) string {
+	return "Hello"
+}
+
+func (instance *intStatus) WebHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WEB HANLDER")
+}
