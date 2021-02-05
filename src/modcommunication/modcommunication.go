8adf0351aff002bf4ca8ca9d5201a6cefd731700
ccoverstreet <coverst2@vols.utk.edu>
2021-02-05 13:28:06 -0500

    Added file saving

--- src/modcommunication/modcommunication.go
+++ src/modcommunication/modcommunication.go
@@ -7,20 +7,75 @@ package modcommunication
 
 import (
 	"sync"
+	"os"
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+
+	"github.com/ccoverstreet/Jablko/src/jlog"
 )
 
 type registryEntry struct {
-	address string
-	authStr string
+	Address string `json:"address"`
+	AuthStr string `json:"authStr"`
 }
 
-type modRegistry struct {
+type ModRegistry struct {
 	sync.Mutex	
 	mods map[string]registryEntry
 }
 
-var moduleRegistry modRegistry
+func (reg *ModRegistry) InitializeRegistry() error {
+	reg.Lock()
+	defer reg.Unlock()
 
-func InitializeRegistry() {
+	reg.mods = make(map[string]registryEntry)
 
+	if _, err := os.Stat("./data/modregistry.json"); os.IsNotExist(err) {
+		jlog.Warnf("Module registry not found in \"data\" directory.\n")
+		jlog.Warnf("Initializing empty registry.\n")
+
+		return nil
+	} else if err != nil {
+		return err
+	}
+
+	regData, err := ioutil.ReadFile("./data/modregistry.json")
+	if err != nil {
+		jlog.Errorf("Unable to retrieve registry data.\n")
+		panic(err)
+	}
+
+	err = json.Unmarshal(regData, &reg.mods)
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
+}
+
+func (reg *ModRegistry) SaveConfig() error {
+	saveData, err := json.MarshalIndent(reg.mods, "", "\t")	
+	if err != nil {
+		panic(err)
+	}
+
+	jlog.Println(string(saveData))
+
+	err = ioutil.WriteFile("./data/modregistry.json", saveData, 0022)
+
+	return err
+}
+
+func (reg *ModRegistry) AddDevice(name string, address string, authStr string) error {
+	jlog.Println(name, address, authStr)
+
+	if _, ok := reg.mods[name]; ok {
+		return fmt.Errorf("Module with the name \"%s\" already exists.", name)
+	}
+
+	reg.mods[name] = registryEntry{address, authStr}
+
+	reg.SaveConfig()
+	return nil	
 }
